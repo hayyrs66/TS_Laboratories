ea2c41b2e541f64c8981f57f88e38c490908d189
hayyrs66 <ricardog.main@gmail.com>
2023-03-25 17:37:53 -0600

    name and student id

--- estructuras_repetitivas/estructuras_repetitivas/Program.cs
+++ estructuras_repetitivas/estructuras_repetitivas/Program.cs
@@ -9,9 +9,11 @@ namespace estructuras_repetitivas
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        { 
+            // Ricardo Guevara 1176323
+
             Ejercicios.Ejercicio1();
-            Ejercicios.Ejercicio2();
+            Ejercicios.Ejercicio2();    
             Ejercicios.Ejercicio3();
             Ejercicios.Ejercicio4();           
 
