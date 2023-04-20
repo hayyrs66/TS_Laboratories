2357140a9832947447d3e9b5dacb8a90b21ba3a1
Ricardo Guevara <ricardog.main@gmail.com>
2023-04-20 12:48:54 -0600

    Update Program.cs

--- arrays_exercise/Program.cs
+++ arrays_exercise/Program.cs
@@ -2,10 +2,9 @@ using System;
 
 class Program {
     static void Main() {
-        // Definimos el vector Valores
-        int[] Valores = { 85, 96, 65, 70, 55, 45, 62, 95, 74, 100, 74, 83, 93 };
         
-        // a. Posiciones de los valores primos
+        int[] Valores = { 85, 96, 65, 70, 55, 45, 62, 95, 74, 100, 74, 83, 93 };
+           
         int[] primos = new int[Valores.Length];
         int countPrimos = 0;
         for (int i = 0; i < Valores.Length; i++) {
@@ -16,8 +15,7 @@ class Program {
         }
         Array.Resize(ref primos, countPrimos);
         Console.WriteLine("Posiciones de los valores primos: " + string.Join(", ", primos));
-        
-        // b. Posiciones de los valores múltiplos de 5
+                
         int[] multiplosDe5 = new int[Valores.Length];
         int countMultiplos = 0;
         for (int i = 0; i < Valores.Length; i++) {
@@ -28,8 +26,7 @@ class Program {
         }
         Array.Resize(ref multiplosDe5, countMultiplos);
         Console.WriteLine("Posiciones de los valores múltiplos de 5: " + string.Join(", ", multiplosDe5));
-        
-        // c. El menor valor del arreglo
+               
         int menor = Valores[0];
         for (int i = 1; i < Valores.Length; i++) {
             if (Valores[i] < menor) {
@@ -38,8 +35,7 @@ class Program {
         }
         Console.WriteLine("El menor valor del arreglo es: " + menor);
     }
-    
-    // Función para verificar si un número es primo
+       
     static bool esPrimo(int num) {
         if (num < 2) {
             return false;
